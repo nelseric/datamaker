c61222b61427a81a6cb7b0504c039949810c721a
nater <wardnath.alt@gmail.com>
2014-08-20 17:47:23 -0400

    Added indicator functions for calculating exponential moving average

--- datamaker/process.py
+++ datamaker/process.py
@@ -7,6 +7,8 @@ import pyximport; pyximport.install()
 
 import datamaker.result.should_buy as should_buy
 
+import datamaker.indicator.EWMA_indicator as EWMA_ind
+
 import IPython
 
 
@@ -28,11 +30,35 @@ def generate_outputs():
 
   print "Applying should_buy"
   data["should_buy"] = should_buy.apply(data.values, 0.00055, 0.00015, 1440)
-
+  
+  
+  
   store.put('ticks_ohlcv', data)
 
   store.close()
+  
+def generate_indicators():
+  if not len(sys.argv) > 1:
+    print "Please provide input database"
+    exit(1)
 
+  input_file_name = sys.argv[1]
+
+  if not os.path.isfile(input_file_name):
+    print "Please provide input database"
+    exit(1)
+
+  store = pd.HDFStore(input_file_name)
+
+  print "Loading 'ticks_ohlcv'"
+  data = store.get('ticks_ohlcv')
+
+  print "Apply dummy indicator calculation"
+  data["EWMA"] = EWMA_ind.apply(data.values)
+  
+  store.put('ticks_ohlcv', data)
+
+  store.close()
 
 
 def shell():
@@ -54,3 +80,6 @@ def shell():
   IPython.embed()
 
   store.close()
+
+if __name__=='__main__':
+    generate_indicators()
