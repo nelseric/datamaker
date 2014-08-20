2766e2ab06fc3c674f58a823b54f4c7cad833b97
nater <wardnath.alt@gmail.com>
2014-08-20 17:45:05 -0400

    Added indicator functions for calculating exponential moving average

---
+++ datamaker/indicator/EWMA_indicator.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 20 14:13:25 2014
+
+@author: nater
+"""
+
+import numpy as np
+import pandas as pd
+
+# 0 Ask         open            1.084150
+# 1             high            1.084150
+# 2             low             1.083600
+# 3             close           1.083900
+# 4 Bid         open            1.084000
+# 5             high            1.084000
+# 6             low             1.083250
+# 7             close           1.083350
+# 8 Bid Volume  Ask volume     70.700001
+# 9 Ask Volume  Bid volume    124.599999
+
+def apply(data,a = .5):
+    
+    #outData = np.zeros((len(data),1))
+    
+    outData = pd.ewma(data[:,0],com=a/(1-a))
+    
+    return outData
