8fc45171e4858384cc9a32bdfc59782524299b8e
nater <wardnath.alt@gmail.com>
2014-09-07 13:53:59 -0400

    got rid of junky comments

--- datamaker/indicator/time_shift_indicator.py
+++ datamaker/indicator/time_shift_indicator.py
@@ -8,17 +8,6 @@ Created on Wed Aug 20 14:13:25 2014
 import numpy as np
 import pandas as pd
 
-# 0 Ask         open            1.084150
-# 1             high            1.084150
-# 2             low             1.083600
-# 3             close           1.083900
-# 4 Bid         open            1.084000
-# 5             high            1.084000
-# 6             low             1.083250
-# 7             close           1.083350
-# 8 Bid Volume  Ask volume     70.700001
-# 9 Ask Volume  Bid volume    124.599999
-
 def calculate(data, shiftList = []):
     outLabel = []
     outData = []
