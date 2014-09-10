11e9b84eb1297691dfd97e5a4da5cb155036abe7
Alex Lagerman <adoghomesliceg@gmail.com>
2014-09-10 14:17:17 -0400

    Hope this stochastic_oscillator meets Eric code nazi standards

---
+++ datamaker/indicator/stochastic_oscillator.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+
+class StochasticOscillator(object):
+
+    #calculates Stochastic Oscillator indicators %K and %D for each span in a given span_list
+    #span_list must contain list of day spans greater than or equal to 3 days
+    def __init__(self, data, span_list):
+        super(StochasticOscillator, self).__init__()
+        self.data = data['Ask_close']
+        self.day_list = span_list
+        self.stoch_data = pd.DataFrame()
+        
+    def calculate(self):
+        for span in self.day_list:
+            minute_span = span * 1440   #converts span in days to minutes
+            min_price = pd.rolling_min(self.data, minute_span, min_periods=1)
+            max_price = pd.rolling_max(self.data, minute_span, min_periods=1)
+            fastk = 100 * ((self.data - min_price)/(max_price - min_price))
+            self.stoch_data[str(span) + 'Day %K'] = fastk
+            #calculates %D using 3day-period SMA of %K
+            fastd = pd.rolling_mean(percent_K, 3*1440, min_periods=1)    # (3*1440) = number of minutes in 3 day period
+            self.stoch_data[str(span) + 'Day %D'] = fastd
+        return self.stoch_data
