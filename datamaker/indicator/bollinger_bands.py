96b79c91743b050786999295d8d03b990402aa3c
Alex Lagerman <adoghomesliceg@gmail.com>
2014-09-10 16:39:02 -0400

    blah

--- datamaker/indicator/bollinger_bands.py
+++ datamaker/indicator/bollinger_bands.py
@@ -17,27 +17,25 @@ class BollingerBands(object):
        bandwidth -- (upperband - lowerband)/middleband
        """
        
-    def __init__(self, data, span, k):
-        super(BollingerBands, self).__init__()
-        self.data = data['Ask_Close']
-        self.day_span = span
-        self.min_span = span * (1440)         #converts days to min: 1440min/day
-        self.k = k
-        self.bb_data = pd.DataFrame()
+       def __init__(self, data, span, k):
+           super(BollingerBands, self).__init__()
+           self.data = data['Ask_Close']
+           self.day_span = span
+           self.min_span = span * (1440)         #converts days to min: 1440min/day
+           self.k = k
+           self.bb_data = pd.DataFrame()
         
-    def calculate(self):
-        span_sma = pd.rolling_mean(self.data, self.min_span, min_periods=1)        
-        span_std = self.k * pd.rolling_std(self.data, self.min_span, min_periods=1)
+       def calculate(self):
+           span_sma = pd.rolling_mean(self.data, self.min_span, min_periods=1)        
+           span_std = self.k * pd.rolling_std(self.data, self.min_span, min_periods=1)
+    
+           self.bb_data[str(self.day_span) + 'Day MidBB'] = span_sma
         
-        self.bb_data[str(self.day_span) + 'Day MidBB'] = span_sma
+           upperbb = span_sma + span_std
+           self.bb_data[str(self.day_span) + 'Day UpperBB'] =  upperbb
         
-        upperbb = span_sma + span_std
-        self.bb_data[str(self.day_span) + 'Day UpperBB'] =  upperbb
+           lowerbb = span_sma - span_std
+           self.bb_data[str(self.day_span) + 'Day LowerBB'] = lowerbb
         
-        lowerbb = span_sma - span_std
-        self.bb_data[str(self.day_span) + 'Day LowerBB'] = lowerbb
-        
-        self.bb_data[str(self.day_span) + 'Day Bandwidth'] = (upperbb - lowerbb)/span_sma
-        return self.bb_data
-
-                
+           self.bb_data[str(self.day_span) + 'Day Bandwidth'] = (upperbb - lowerbb)/span_sma
+           return self.bb_data
