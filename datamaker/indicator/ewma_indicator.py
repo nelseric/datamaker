35b6cc68133b4b2948bd6a4796e142de37ec9200
Alex Lagerman <adoghomesliceg@gmail.com>
2014-09-10 16:52:47 -0400

    edited EWMA indicator

---
+++ datamaker/indicator/ewma_indicator.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+import random
+
+class EWMAIndicator(object):
+
+    #takes a list of k spans and calculates EWMA
+    def __init__(self, data, k):
+        super(EWMAIndicator, self).__init__()
+        self.data = data['Ask_open']
+        self.span_list = k
+        self.outData = pd.DataFrame()
+        
+    def calculate(self):
+        for span in self.span_list:
+            self.outData[str(span) + 'Day EWMA'] = pd.ewma(self.data, span=span)
+        return self.outData
+
+
+
