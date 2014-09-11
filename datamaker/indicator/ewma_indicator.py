5a66516bcbf9db2534776e9b334ee8cd12c98f64
wardnath <wardnath@msu.edu>
2014-09-11 17:40:04 +0000

    num spans to span_container

--- datamaker/indicator/ewma_indicator.py
+++ datamaker/indicator/ewma_indicator.py
@@ -2,10 +2,10 @@ import pandas as pd
 
 class EWMAIndicator(object):
     """takes a list of k spans and calculates Exponential Weighted Moving Average"""
-    def __init__(self, data, num_spans):
+    def __init__(self, data, span_container):
         super(EWMAIndicator, self).__init__()
         self.data = data
-        self.span_list = num_spans
+        self.span_list = span_container
         self.outData = pd.DataFrame()
         
     def calculate(self):
