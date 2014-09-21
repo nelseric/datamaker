df9bb89058f670f49d4d8e7310405ab4ea7248ad
Eric Nelson <eric@clean-logix.com>
2014-09-21 16:39:46 -0400

    Experiment feature instantiation

--- datamaker/experiment.py
+++ datamaker/experiment.py
@@ -1,14 +1,38 @@
 from datamaker.indicator import *
+from datamaker.result import *
+
+from datamaker.feature import Feature
+import json
+import types
+import pandas as pd
 
 class Experiment(object):
   """Experiment class, will parse experiment data, and generate stuff"""
-  def __init__(self):
-    super(Experiment, self).__init__()
-    ewma.NormalizedEWMA
 
   @staticmethod
-  def load_file(experiment_file):
-    pass
+  def load(experiment_json):
+    expmt = json.load(experiment_json)
+    return Experiment(**expmt)
 
-  def things(self):
-    return [ewma.NormalizedEWMA, macd.MACD]
+  def __init__(self, *args, **kwargs):
+    super(Experiment, self).__init__()
+    self.name = kwargs.get('name')
+    self.description = kwargs.get('description')
+    self.tick_file = kwargs.get('tick_file')
+    self.instrument = kwargs.get('instrument')
+    self.features = map(self.parse_feature, kwargs.get("features", []))
+    self.classes = map(self.parse_feature, kwargs.get("classes", []))
+
+  def parse_feature(self, feature_data):
+    klass = feature_data["class"].split('.')
+    cur = globals()[klass.pop(0)]
+    while isinstance(cur, types.ModuleType):
+      cur = getattr(cur, klass.pop(0))
+
+    params = feature_data.get("parameters", {})
+    params["data"] = self.get_input_data()
+
+    return cur(**params)
+
+  def get_input_data(self):
+    return pd.DataFrame([1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0])
--- test/experiment_test.py
+++ test/experiment_test.py
@@ -3,7 +3,6 @@ from datamaker.experiment import Experiment
 
 def test_module_import():
   assert Experiment
-  assert Experiment().things()
 
 def test_experiment_parse():
-  experiment = Experiment.load_file("fixtures/experiment.json")
+  experiment = Experiment.load(open("test/fixtures/experiment.json"))
