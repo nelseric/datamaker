ef4041619bddd7ab613238ee84d34c044a1ee413
Eric Nelson <eric@clean-logix.com>
2014-09-21 12:38:55 -0400

    Updated h2o, added oandapy, added start of experiment

---
+++ datamaker/experiment.py
@@ -0,0 +1,8 @@
+class Experiment(object):
+  """Experiment class, will parse experiment data, and generate stuff"""
+  def __init__(self):
+    super(Experiment, self).__init__()
+
+  @staticmethod
+  def load_file(experiment_file):
+    pass
