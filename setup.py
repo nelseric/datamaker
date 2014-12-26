0a066e72b785279f36ffb1d0a7d04cec95b2515d
Eric Nelson <eric@clean-logix.com>
2014-12-26 11:15:10 -0500

    Moving to 4 spaces formatting. anything you touch should match it

--- setup.py
+++ setup.py
@@ -6,43 +6,46 @@ from Cython.Build import cythonize
 import numpy
 import sys
 
+
 class PyTest(TestCommand):
-  user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
 
-  def initialize_options(self):
-    TestCommand.initialize_options(self)
-    self.pytest_args = []
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
 
-  def finalize_options(self):
-    TestCommand.finalize_options(self)
-    self.test_args = []
-    self.test_suite = True
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
 
-  def run_tests(self):
-    #import here, cause outside the eggs aren't loaded
-    import pytest
-    errno = pytest.main(self.pytest_args)
-    sys.exit(errno)
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
 
 setup(
-  name='datamaker',
-  version='0.0.1',
-  packages=find_packages(),
-  url='',
-  license='',
-  author='Eric Nelson',
-  author_email='gauntletguy2@gmail.com',
-  description='',
-  ext_modules=cythonize("**/*.pyx"),
-  include_dirs=[numpy.get_include()],
-  tests_require=['pytest', 'pytest-cov'],
-  cmdclass={'test': PyTest},
+    name='datamaker',
+    version='0.0.2',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='Eric Nelson',
+    author_email='gauntletguy2@gmail.com',
+    description='',
+    ext_modules=cythonize("**/*.pyx"),
+    include_dirs=[numpy.get_include()],
+    tests_require=['pytest', 'pytest-cov'],
+    cmdclass={'test': PyTest},
 
-  entry_points={
-    'console_scripts': [
-      'dm = datamaker.bin:dm_main',
-      'dm-backtest = datamaker.bin:run_backtest',
-      'idle = idlelib.PyShell:main'
-      ]
+    entry_points={
+        'console_scripts': [
+            'dm = datamaker.bin:dm_main',
+            'dm-backtest = datamaker.bin:run_backtest',
+            'idle = idlelib.PyShell:main'
+        ]
     }
 )
