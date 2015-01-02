9ada7520f20168fa3e14d1390c268c9ff4c86268
Nate Ward <wardnath@msu.edu>
2015-01-02 13:27:33 -0500

    added training, preprocessing, saving

--- datamaker/bin/gen_models.py
+++ datamaker/bin/gen_models.py
@@ -1,5 +1,6 @@
 """ Generating Models """
-
+import numpy as np
+import pandas as pd
 from pathlib import Path
 import json
 
@@ -7,27 +8,28 @@ import datamaker.db as db
 import datamaker.model as mlmod
 
 
-def gen_models(path=Path("."), feature_data):
+def gen_models(path=Path(".")):
     """ 
         Train the models and then save them for later
     """
 
-    
-    
-    for model_file in path.glob("models/*.json"):
-        # Load data
-        model_params = json.load(model_file.open())
+    strategy_params = json.load((path / 'project.json').open())
 
-        # Initialize model
-        model_inst = getattr(mlmod,model_params['model_type'])()
+    for strat_element in strategy_params['strategies']:
+        for model_file in path.glob("models/*.json"):
+            # Load data
+            model_params = json.load(model_file.open())
 
-        # Train model on data
-        model_inst.train(model_params, feature_data)
 
-        # Save model
+            # Initialize model
+            model_inst = getattr(mlmod,model_params['model_type'])(path)
 
-        import IPython
-        IPython.embed()
+            # Train model on data
+            feature_data = np.zeros([100,5])
+            model_inst.train(model_params, feature_data, strat_element)
+
+
+        
 
 
 
--- datamaker/model.py
+++ datamaker/model.py
@@ -1,8 +1,15 @@
 """
   ML Model Training and prediction
 """
+
+import numpy as np
+import pandas as pd
 from sklearn.ensemble import BaggingClassifier
 from sklearn.ensemble import ExtraTreesClassifier
+from sklearn import preprocessing
+from sklearn.externals import joblib
+import os
+
 
 class Model(object):
 
@@ -10,25 +17,27 @@ class Model(object):
     Parent class for all machine learning classfiers and regressors
     """
 
-    def __init__(self):
+    def __init__(self, path):
         super(Model, self).__init__()
-        
+        self.path = path
 
-    def save_model(self):
-        """Saves the model as a pickled file"""
-
-    def load_model(self):
+    def load_model(self, model_name):
         """Loads the model from a pickled file"""
 
     def _preprocess(self, feature_data):
         """
-        Makes the data ready for classification
-        see: http://scikit-learn.org/0.11/modules/preprocessing.html
+        Makes the data ready for classification; specifically this will 
+        normalize the data and 
+        see: http://scikit-learn.org/stable/modules/preprocessing.html
         """
 
-        
+        # Scale the data first (zero mean, unit variance)
+        feature_data = preprocessing.scale(feature_data)
 
+        # Remove rows with NaN values within
+        feature_data = feature_data[~np.isnan(feature_data).any(axis=1)]
 
+        return feature_data
 
 
 class ETCModel(Model):
@@ -36,23 +45,34 @@ class ETCModel(Model):
     """
     The Extremely Random Forest Classifier (ExtraTreesClassifier)
     """
-    def __init__(self):
-        super(ETCModel, self).__init__()
 
-    def train(self, model_params, feature_data):
+    def __init__(self, path):
+        super(ETCModel, self).__init__(path)
+
+    def train(self, model_params, feature_data, strategy_params):
         """
         Trains the ETC Model
         """
 
-        #Instantiate the models
+        # Instantiate the models
         mod_inst = ExtraTreesClassifier()
         mod_inst.set_params(**model_params['all_params']['model_params'])
         meta_est = BaggingClassifier(mod_inst)
         meta_est.set_params(**model_params['all_params']['bagging_params'])
 
-        #Preprocess the data
-        _preprocess(feature_data)
+        # Preprocess the data
+        feature_data = self._preprocess(feature_data)
 
+        # Train the model
+        meta_est.fit(feature_data[:, 0:-2], feature_data[:, -1])
 
+        # Save the model
+        self.save_model(meta_est, strategy_params)
 
-
+    def save_model(self, ml_mod, strategy_params):
+        """Saves the model as a pickled file"""
+        import IPython
+        IPython.embed()
+        pkl_path = 'models/' + strategy_params['instrument'] + '_ETC/'
+        os.mkdir(pkl_path)
+        joblib.dump(ml_mod, pkl_path + 'model.pkl')
